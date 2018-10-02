0f86f8befda78a5029247efa0fb2edae0e6107d0
su-kun1899 <higedrum.coz@gmail.com>
2018-10-02 21:36:47 +0900

    ネストしたプロパティだけvarにすれば逃げられる。。

--- src/main/kotlin/red/sukun1899/kotlin/mybatis/sandbox/repository/SampleRepository.kt
+++ src/main/kotlin/red/sukun1899/kotlin/mybatis/sandbox/repository/SampleRepository.kt
@@ -12,11 +12,12 @@ interface SampleRepository {
     // N+1は起きるが、MyBatisのconstructorで解決できる
     fun findAll(): List<Sample>
 
+    // 入れ子だけvarにする逃げの一手
     fun findSample2(): List<Sample2>
 }
 
 data class Sample(val id: String, val name: String, val childNames: List<String>)
 
-data class Sample2(val id: String, val name: String) {
-    var childNames = mutableListOf<String>()
+data class Sample2(val id: String, val name: String, var childNames: List<String>) {
+    constructor(id: String, name: String) : this(id, name, mutableListOf())
 }
