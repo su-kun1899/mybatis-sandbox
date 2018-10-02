21082b5afb91b4c30bcd2beb5bf2744a2d4bbc4c
su-kun1899 <higedrum.coz@gmail.com>
2018-10-02 20:08:22 +0900

    SQLを実装

--- src/test/groovy/red/sukun1899/kotlin/mybatis/sandbox/repository/SampleRepositorySpec.groovy
+++ src/test/groovy/red/sukun1899/kotlin/mybatis/sandbox/repository/SampleRepositorySpec.groovy
@@ -1,8 +1,13 @@
 package red.sukun1899.kotlin.mybatis.sandbox.repository
 
+import com.ninja_squad.dbsetup.DbSetup
+import com.ninja_squad.dbsetup.Operations
+import com.ninja_squad.dbsetup.destination.DataSourceDestination
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
+
+import javax.sql.DataSource
 
 /**
  * @author su-kun1899
@@ -12,9 +17,29 @@ class SampleRepositorySpec extends Specification {
     @Autowired
     SampleRepository sampleRepository
 
-    def "Hello, mybatis"() {
-        expect:
-        sampleRepository.findAll().size() == 0
+    @Autowired
+    DataSource dataSource
+
+    DataSourceDestination dataSourceDestination
+
+    def setup() {
+        dataSourceDestination = new DataSourceDestination(dataSource)
     }
 
+    def "Hello, mybatis"() {
+        given:
+        def parentId = UUID.randomUUID().toString()
+        def insertParent = Operations.insertInto("parent")
+                .columns("id", "name")
+                .values(parentId, "parent1")
+                .build()
+        def operations = Operations.sequenceOf(insertParent)
+        new DbSetup(dataSourceDestination, operations).launch()
+
+        expect:
+        sampleRepository.findAll().size() == 1
+
+        cleanup:
+        new DbSetup(dataSourceDestination, Operations.deleteAllFrom("parent")).launch()
+    }
 }
