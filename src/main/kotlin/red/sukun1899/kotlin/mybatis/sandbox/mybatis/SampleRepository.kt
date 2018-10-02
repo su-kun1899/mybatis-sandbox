9d564d6d57497db236d1a1813bccb2f65a2ba4b2
su-kun1899 <higedrum.coz@gmail.com>
2018-10-02 19:41:31 +0900

    Hello, mybatis

---
+++ src/main/kotlin/red/sukun1899/kotlin/mybatis/sandbox/mybatis/SampleRepository.kt
@@ -0,0 +1,15 @@
+package red.sukun1899.kotlin.mybatis.sandbox.mybatis
+
+import org.apache.ibatis.annotations.Mapper
+import org.springframework.stereotype.Repository
+
+/**
+ * @author su-kun1899
+ */
+@Mapper
+@Repository
+interface SampleRepository {
+    fun findAll(): List<Sample>
+}
+
+data class Sample(val id: String)
---
+++ src/test/groovy/red/sukun1899/kotlin/mybatis/sandbox/mybatis/SampleRepositorySpec.groovy
@@ -0,0 +1,20 @@
+package red.sukun1899.kotlin.mybatis.sandbox.mybatis
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+/**
+ * @author su-kun1899
+ */
+@SpringBootTest
+class SampleRepositorySpec extends Specification {
+    @Autowired
+    SampleRepository sampleRepository
+
+    def "Hello, mybatis"() {
+        expect:
+        sampleRepository.findAll().size() == 0
+    }
+
+}
