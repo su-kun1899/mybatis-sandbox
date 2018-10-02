59c0a21c3f57c567590e3f6e652001db0178c091
su-kun1899 <higedrum.coz@gmail.com>
2018-10-02 19:12:34 +0900

    Hello, spock

---
+++ src/test/groovy/red/sukun1899/kotlin/mybatis/sandbox/HelloSpec.groovy
@@ -0,0 +1,13 @@
+package red.sukun1899.kotlin.mybatis.sandbox
+
+import spock.lang.Specification
+
+/**
+ * @author su-kun1899
+ */
+class HelloSpec extends Specification {
+    def "Hello"() {
+        expect:
+        true
+    }
+}
