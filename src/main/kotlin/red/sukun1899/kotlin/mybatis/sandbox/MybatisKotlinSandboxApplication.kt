b05f5405d9714a008a361c51de02f49cefba43b1
su-kun1899 <higedrum.coz@gmail.com>
2018-10-02 18:57:57 +0900

    Initial commit

---
+++ src/main/kotlin/red/sukun1899/kotlin/mybatis/sandbox/MybatisKotlinSandboxApplication.kt
@@ -0,0 +1,11 @@
+package red.sukun1899.kotlin.mybatis.sandbox
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MybatisKotlinSandboxApplication
+
+fun main(args: Array<String>) {
+    runApplication<MybatisKotlinSandboxApplication>(*args)
+}
---
+++ src/test/kotlin/red/sukun1899/kotlin/mybatis/sandbox/MybatisKotlinSandboxApplicationTests.kt
@@ -0,0 +1,16 @@
+package red.sukun1899.kotlin.mybatis.sandbox
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class MybatisKotlinSandboxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
